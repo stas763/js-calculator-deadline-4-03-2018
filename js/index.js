5e2267254516380ec9cedca96b1716350c460b45
stan kleshnov <s.v.kleshnov@gmail.com>
2018-04-04 07:37:11 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
-function plus() {
-    const num1, num2;
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    result = num1+num2;
-    document.getElementById('out').innerHTML = result;
-}
-function minus() {
-    const num1, num2;
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    result = num1-num2;
-    document.getElementById('out').innerHTML = result;
-}
+// function plus() {
+//     const num1, num2;
+//     num1 = document.getElementById('n1').value;
+//     num1 = parseInt(num1);
+//     num2 = document.getElementById('n2').value;
+//     num2 = parseInt(num2);
+//     result = num1+num2;
+//     document.getElementById('out').innerHTML = result;
+// }
+// function minus() {
+//     const num1, num2;
+//     num1 = document.getElementById('n1').value;
+//     num1 = parseInt(num1);
+//     num2 = document.getElementById('n2').value;
+//     num2 = parseInt(num2);
+//     result = num1-num2;
+//     document.getElementById('out').innerHTML = result;
+// }
+
+document.getElementById('seven').addEventListener('click', function(e) {
+  document.getElementById('display').value += '7';
+});
